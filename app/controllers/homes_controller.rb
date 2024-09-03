944d5f39a9fb96043db3d29843a6d95e4b7a0d81
Leilanigreer <leilani@lpcgolf.com>
2024-09-02 18:30:12 -0700

    added hard coded show in controller and routes

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,4 +4,10 @@ class HomesController < ApplicationController
     @homes = Home.all 
     render template: "homes/index"
   end
+
+  def show
+    # render json: {message: "hello"}
+    @home = Home.find_by(id: 2)
+    render template: "homes/show"
+  end
 end
